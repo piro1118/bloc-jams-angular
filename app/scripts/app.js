63d50063e1ea745e339886023430fc85fe3864f6
Hiroyuki Ishikawa <ishikawa.hiroyuki1118@gmail.com>
2017-08-06 14:45:49 +0900

    Checkpoint 3 routing: revised and completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,23 @@
 (function() {
     function config($stateProvider, $locationProvider) {
         $locationProvider
-    .html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+            .html5Mode({
+                    enabled: true,
+                    requireBase: false
+            });
 
-    $stateProvider
-        .state('landing', {
-            url: '/',
-            templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        });
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
 
     }
 
-angular.module('blocJams', ['ui.router']);
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
