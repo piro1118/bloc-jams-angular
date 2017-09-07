cb707c8153a2c9f5642d26c8059d12a8ed24ba83
Hiroyuki Ishikawa <ishikawa.hiroyuki1118@gmail.com>
2017-09-07 23:53:06 +0900

    Checkpoint 10 directives2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
 
          /**
@@ -58,6 +58,12 @@
                 preload: true
             });
 
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+
             SongPlayer.currentSong = song;
          };
 
@@ -66,6 +72,12 @@
          * @type {Object}
          */
          SongPlayer.currentSong = null;
+
+         /**
+         * @desc Current playback time (in seconds) of currently playing song
+         * @type {Number}
+         */
+         SongPlayer.currentTime = null;
 
          /**
          * @function play
@@ -131,10 +143,23 @@
              }
          };
 
+         /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+
+
          return SongPlayer;
     }
 
+
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
