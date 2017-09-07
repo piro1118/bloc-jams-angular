5034e25969467fe8efe354c96b4731495c5d2a98
Hiroyuki Ishikawa <ishikawa.hiroyuki1118@gmail.com>
2017-09-08 00:06:44 +0900

    Assignment 10 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -80,6 +80,12 @@
          SongPlayer.currentTime = null;
 
          /**
+         * @desc volume for the song
+         * @type {Number}
+         */
+         SongPlayer.volume = 80;
+
+         /**
          * @function play
          * @desc Play current or new song
          * @param {Object} song
@@ -154,6 +160,17 @@
             }
         };
 
+        /**
+        * @function setVolume
+        * @desc Set volume for songs
+        * @param {Number} volume
+        */
+        SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume);
+            }
+            SongPlayer.volume = volume;
+        };
 
          return SongPlayer;
     }
